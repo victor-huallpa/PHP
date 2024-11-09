25ac31a9613445d9e2007481ee93aa2dcdac6151
victor-huallpa <vech.600@gmail.com>
2024-11-09 14:32:16 -0500

    OPERADORES ARITMETICOS, ASGINACION, COMPARACION Y REFERENCIA

---
+++ phpBasic\/8_operadores_asignacion.php
@@ -0,0 +1,41 @@
+<?php
+/*
+El signo de asignar es el igual '=', a este signo s ele puede anteponer algunos operadores matematicos y el de concatenaicon.
+Esto ayuda a realizar las operaciones de manera mas corata
+-=
++=
+/=
+*=
+.=
+*/
+#varaibles
+$val1 = 2;
+$val2 = 24;
+
+#RESTA
+$val1 -= 4;
+echo $val1,'<br/>';
+
+#SUMA
+$val1 += 4;
+echo $val1,'<br/>';
+
+#MULTIPLICACION
+$val1 *= 4;
+echo $val1,'<br/>';
+
+#DIVICION
+$val1 /= 4;
+echo $val1,'<br/>';
+
+#CONCATENACION
+$val1 .= 4;
+echo $val1,'<br/>';
+
+#RESTO
+$val1 %= 7;
+echo $val1,'<br/>';
+
+#EXPONENTE
+$val1 **= 4;
+echo $val1,'<br/>';
