4110d302fdff903361c298cde799f105e226bb6a
victor-huallpa <vech.600@gmail.com>
2025-04-11 22:56:09 -0500

    funcionalidad

--- others/senati/tr_senati_ultimo/public/index.php
+++ others/senati/tr_senati_ultimo/public/index.php
@@ -4,7 +4,7 @@
     include_once '../config/error.php';
     include_once '../config/session-start.php';
     //RUTA DE LA VARIABLE VIEW DE INDEX
-    $url = isset($_GET['views']) && !empty($_GET['views']) ? explode("/", $_GET['views']) : ['login'];
+    $url = isset($_GET['views']) && !empty($_GET['views']) ? explode("/", $_GET['views']) : ['login']; 
     //INCLUCION DE CONTENIDO
     include_once '../app/view/inc/head.php';
     include_once '../app/view/inc/body.php';
